76d92402a080159e96749d2c4144f1cb48fc2b5d
Ziggyboi <dumbleshoot@gmail.com>
2019-06-05 04:42:22 +0100

    Adding my friends view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
         @friendships = current_user.friends
     end
 
+    def search
+        if params[:search_param].blank?
+            flash.now[:danger] = "You have entered an empty search string"
+        else
+            @users = User.search(params[:search_param])
+            flash.now[:danger] = "No users match your search criteria" unless @users
+        end
+        respond_to do |format|
+            format.js { render partial: 'friends/result' }
+        end
+    end
 end
